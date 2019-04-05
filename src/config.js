100344b25eaafe824ae1f99a72acf148256667fc
Yauheni23 <yauheni.yarmolich@mail.ru>
2019-04-05 15:07:08 +0300

    ref: add config.js

---
+++ src/config.js
@@ -0,0 +1,42 @@
+export const config =  {
+    MONTH: ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun', 'Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+    DAYS_OF_WEEK: ['Su', 'Mo', 'Tu', 'We', 'Th', 'Fr', 'Sa'],
+
+    SELECTOR_INPUT_DATE_PICKER: 'input[type="date_picker"]',
+    SELECTOR_DIV: 'div',
+    SELECTOR_SELECT: 'select',
+    SELECTOR_OPTION: 'option',
+    SELECTOR_INPUT: 'INPUT',
+    SELECTOR_SPAN: 'span',
+    SELECTOR_BODY: 'body',
+
+    EVENT_LISTENER_MOUSEDOWN: 'mousedown',
+    EVENT_LISTENER_CHANGE: 'change',
+    EVENT_LISTENER_FOCUS: 'focus',
+    EVENT_LISTENER_BLUR: 'blur',
+    EVENT_LISTENER_CLICK: 'click',
+
+    CSS_CLASS_CALENDAR: 'calendar',
+    CSS_CLASS_DATE_INPUT_WRAPPER: 'dateInputWrapper',
+    CSS_CLASS_ROW_DAYS_OF_MONTH: 'rowDaysOfMonth',
+    CSS_CLASS_DAYS_OF_MONTH: 'daysOfMonth',
+    CSS_CLASS_DAY_OF_MONTH: 'dayOfMonth',
+    CSS_CLASS_DAYS_OF_WEEK: 'daysOfWeek',
+    CSS_CLASS_DAY_OF_WEEK: 'dayOfWeek',
+    CSS_CLASS_TODAY: 'today',
+    CSS_CLASS_ACTIVE: 'active',
+    CSS_CLASS_ENABLED: 'enabled',
+    CSS_CLASS_SELECTED: 'selected',
+
+    CSS_ID_MONTH: 'month',
+    CSS_ID_YEAR: 'year',
+
+    ATTRIBUTE_TYPE_NUMBER: 'number',
+
+};
+
+
+
+
+
+
--- src/index.js
+++ src/index.js
@@ -1,29 +1,5 @@
 import './style.css';
-
-const Month = [
-    'Jan',
-    'Feb',
-    'Mar',
-    'Apr',
-    'May',
-    'Jun',
-    'Jul',
-    'Aug',
-    'Sep',
-    'Oct',
-    'Nov',
-    'Dec'
-];
-
-const DaysOfWeek = [
-    'Su',
-    'Mo',
-    'Tu',
-    'We',
-    'Th',
-    'Fr',
-    'Sa',
-];
+import {config} from './config';
 
 Date.prototype.daysInMonth = function() {
     return 33 - new Date(this.getFullYear(), this.getMonth(), 33).getDate();
@@ -32,7 +8,7 @@ Date.prototype.daysInMonth = function() {
 document.addEventListener('DOMContentLoaded', function() {
 
 
-document.querySelectorAll('input[type="date_picker"]').forEach((datepicker, index) => {
+document.querySelectorAll(config.SELECTOR_INPUT_DATE_PICKER).forEach((datepicker, index) => {
     let clickDatepicker = false;
 
     datepicker.dataset.id = index + '';
@@ -42,23 +18,23 @@ document.querySelectorAll('input[type="date_picker"]').forEach((datepicker, inde
 
     const coordinates = datepicker.getBoundingClientRect();
 
-    const calendar = document.createElement('div');
-    calendar.className = 'calendar';
+    const calendar = document.createElement(config.SELECTOR_DIV);
+    calendar.className = config.CSS_CLASS_CALENDAR;
 
 
     calendar.style.top = `${coordinates.bottom}px`;
     calendar.style.left = `${coordinates.left}px`;
 
-    const dateInputWrapper = document.createElement('div');
+    const dateInputWrapper = document.createElement(config.SELECTOR_DIV);
 
-    dateInputWrapper.className = 'dateInputWrapper';
+    dateInputWrapper.className = config.CSS_CLASS_DATE_INPUT_WRAPPER;
 
-    const selectMonth = document.createElement('select');
+    const selectMonth = document.createElement(config.SELECTOR_SELECT);
 
-    selectMonth.id = 'month';
+    selectMonth.id = config.CSS_ID_MONTH;
 
-    Month.forEach((el, index) => {
-        const option = document.createElement('option');
+    config.MONTH.forEach((el, index) => {
+        const option = document.createElement(config.SELECTOR_OPTION);
         option.value = `${index}`;
         option.innerText = el;
         selectMonth.appendChild(option);
@@ -66,21 +42,21 @@ document.querySelectorAll('input[type="date_picker"]').forEach((datepicker, inde
 
     selectMonth.value = currentDate.getMonth() + '';
 
-    const inputYear = document.createElement('input');
+    const inputYear = document.createElement(config.SELECTOR_INPUT);
 
-    inputYear.id = 'year';
-    inputYear.type = "number";
+    inputYear.id = config.CSS_ID_YEAR;
+    inputYear.type = config.ATTRIBUTE_TYPE_NUMBER;
     inputYear.value = currentDate.getFullYear() + '';
 
-    const daysOfWeek = document.createElement('div');
+    const daysOfWeek = document.createElement(config.SELECTOR_DIV);
 
-    daysOfWeek.className = 'daysOfWeek';
+    daysOfWeek.className = config.CSS_CLASS_DAYS_OF_WEEK;
 
-    DaysOfWeek.forEach(name => {
-        const dayOfWeek = document.createElement('div');
-        dayOfWeek.className= 'dayOfWeek';
+    config.DAYS_OF_WEEK.forEach(name => {
+        const dayOfWeek = document.createElement(config.SELECTOR_DIV);
+        dayOfWeek.className= config.CSS_CLASS_DAY_OF_WEEK;
 
-        const spanDayOfWeek = document.createElement('span');
+        const spanDayOfWeek = document.createElement(config.SELECTOR_SPAN);
         spanDayOfWeek.innerText = name;
 
         dayOfWeek.appendChild(spanDayOfWeek);
@@ -89,8 +65,8 @@ document.querySelectorAll('input[type="date_picker"]').forEach((datepicker, inde
 
     const daysOfMonth = createDaysOfMonth(currentDate);
     daysOfMonth.dataset.id = index + '';
-    selectDate(daysOfMonth);
 
+    selectDate(daysOfMonth);
 
     dateInputWrapper.appendChild(selectMonth);
 
@@ -102,51 +78,59 @@ document.querySelectorAll('input[type="date_picker"]').forEach((datepicker, inde
 
     calendar.appendChild(daysOfMonth);
 
-    document.querySelector("body").appendChild(calendar);
+    document.querySelector(config.SELECTOR_BODY).appendChild(calendar);
 
 
 
 
 
-    calendar.addEventListener("mousedown", () => {
+    calendar.addEventListener(config.EVENT_LISTENER_MOUSEDOWN, () => {
         clickDatepicker = true;
     });
 
-    selectMonth.addEventListener('change', () => {
+    selectMonth.addEventListener(config.EVENT_LISTENER_CHANGE, () => {
         daysOfMonth.innerHTML = createDaysOfMonth(new Date(+inputYear.value, +selectMonth.value)).innerHTML;
         selectDate(daysOfMonth);
     });
 
-    inputYear.addEventListener('change', () => {
+    inputYear.addEventListener(config.EVENT_LISTENER_CHANGE, () => {
         daysOfMonth.innerHTML = createDaysOfMonth(new Date(+inputYear.value, +selectMonth.value)).innerHTML;
         selectDate(daysOfMonth);
     });
 
 
-    datepicker.addEventListener('focus', () => {
-        calendar.classList.add('active');
+    datepicker.addEventListener(config.EVENT_LISTENER_FOCUS, () => {
+        calendar.classList.add(config.CSS_CLASS_ACTIVE);
     });
 
-    datepicker.addEventListener('blur', (event) => {
+    datepicker.addEventListener(config.EVENT_LISTENER_BLUR, (event) => {
 
-        if(clickDatepicker) datepicker.focus()
+        if(clickDatepicker) { datepicker.focus(); }
             else {
-                calendar.classList.remove('active');
+                calendar.classList.remove(config.CSS_CLASS_ACTIVE);
                 const selectedDate = datepicker.value.split('-');
 
                 selectedDate[1] = `${+selectedDate[1] -1}`;
 
-                if(+selectedDate[1] < 10) selectedDate[1] = '0' + selectedDate[1];
+                if(+selectedDate[1] < 10) { selectedDate[1] = '0' + selectedDate[1]; }
 
-                const checkSelectedDate = (+selectedDate[0] >= 1000) && (+selectedDate[0] < 9999) && (+selectedDate[1] >= 1) && (+selectedDate[1] <= 12) && (+selectedDate[2] >= 1) && (+selectedDate[2] <= new Date(+selectedDate[0], +selectedDate[1]).daysInMonth());
-                if (selectedDate && checkSelectedDate && !Number.isNaN(+selectedDate[0] + +selectedDate[1] + +selectedDate[2])){
-                    daysOfMonth.innerHTML = createDaysOfMonth(new Date(+selectedDate[0], +selectedDate[1])).innerHTML;
-                    selectDate(daysOfMonth);
-                    inputYear.value = selectedDate[0];
-                    selectMonth.value = `${+selectedDate[1]}`;
-                    daysOfMonth.querySelector(`div[data-date="${selectedDate[0]}-${selectedDate[1]}-${selectedDate[2]}"]`).classList.add('selected');
+            const checkSelectedDate = (+selectedDate[0] >= 1000)
+                    && (+selectedDate[0] < 9999)
+                    && (+selectedDate[1] >= 1)
+                    && (+selectedDate[1] <= 12)
+                    && (+selectedDate[2] >= 1)
+                    && (+selectedDate[2] <= new Date(+selectedDate[0], +selectedDate[1]).daysInMonth());
+
+                if (selectedDate
+                    && checkSelectedDate
+                    && !Number.isNaN(+selectedDate[0] + +selectedDate[1] + +selectedDate[2])) {
+
+                        daysOfMonth.innerHTML = createDaysOfMonth(new Date(+selectedDate[0], +selectedDate[1])).innerHTML;
+                        selectDate(daysOfMonth);
+                        inputYear.value = selectedDate[0];
+                        selectMonth.value = `${+selectedDate[1]}`;
+                        daysOfMonth.querySelector(`div[data-date="${selectedDate[0]}-${selectedDate[1]}-${selectedDate[2]}"]`).classList.add('selected');
                 }
-
         }
 
         clickDatepicker = false;
@@ -159,23 +143,23 @@ document.querySelectorAll('input[type="date_picker"]').forEach((datepicker, inde
 
 
 function createDaysOfMonth( date ) {
-    const daysOfMonth = document.createElement('div');
-    daysOfMonth.className = 'daysOfMonth';
+    const daysOfMonth = document.createElement(config.SELECTOR_DIV);
+    daysOfMonth.className = config.CSS_CLASS_DAYS_OF_MONTH;
 
     const dayOfWeek = new Date(date.getFullYear(), date.getMonth()).getDay();
     const countWeeksInMonth = (date.daysInMonth() - 7 + dayOfWeek) / 7 | 0 ;
 
     for(let i = 0; i <= countWeeksInMonth + 1; i++) {
-        const rowDaysOfMonth = document.createElement('div');
-        rowDaysOfMonth.className = 'rowDaysOfMonth';
+        const rowDaysOfMonth = document.createElement(config.SELECTOR_DIV);
+        rowDaysOfMonth.className = config.CSS_CLASS_ROW_DAYS_OF_MONTH;
 
         for(let j = 0; j < 7; j++){
-            const dayOfMonth = document.createElement('div');
-            dayOfMonth.className = 'dayOfMonth';
+            const dayOfMonth = document.createElement(config.SELECTOR_DIV);
+            dayOfMonth.className = config.CSS_CLASS_DAY_OF_MONTH;
 
             const currentDayOfMonth = (7 * i) + j + 1 - dayOfWeek;
 
-            const spanDayOfMonth = document.createElement('span');
+            const spanDayOfMonth = document.createElement(config.SELECTOR_SPAN);
 
             const day = (currentDayOfMonth > 0 && currentDayOfMonth <= date.daysInMonth()) ?
                 currentDayOfMonth + '' : '';
@@ -183,7 +167,7 @@ function createDaysOfMonth( date ) {
             spanDayOfMonth.innerText = day;
 
             if(day !== '') {
-                dayOfMonth.classList.add('enabled');
+                dayOfMonth.classList.add(config.CSS_CLASS_ENABLED);
                 dayOfMonth.dataset.date = `${date.getFullYear()}-${(date.getMonth() < 9) ? '0' + date.getMonth() : date.getMonth() }-${(day <= 9) ? '0' + day : day }`;
             }
 
@@ -195,9 +179,10 @@ function createDaysOfMonth( date ) {
     }
 
     if(new Date().getFullYear() === date.getFullYear() && new Date().getMonth() === date.getMonth()){
-        daysOfMonth.querySelectorAll('.dayOfMonth.enabled').forEach( el => {
+        daysOfMonth.querySelectorAll(`.${config.CSS_CLASS_DAY_OF_MONTH}.${config.CSS_CLASS_ENABLED}`)
+            .forEach( el => {
             if(el.childNodes && +el.childNodes[0].innerText === new Date().getDate()){
-                el.classList.add('today');
+                el.classList.add(config.CSS_CLASS_TODAY);
             }
         })
     }
@@ -210,22 +195,27 @@ function selectDate(daysOfMonth) {
     daysOfMonth.childNodes.forEach(week => {
         week.childNodes.forEach( day => {
             if (day.dataset.date && day.dataset.date !==''){
-                day.addEventListener('click', () => {
-                    if (daysOfMonth.querySelector('.rowDaysOfMonth .dayOfMonth.selected'))
-                        daysOfMonth.querySelector('.rowDaysOfMonth .dayOfMonth.selected').classList.remove('selected');
+                day.addEventListener(config.EVENT_LISTENER_CLICK, () => {
+                    if (daysOfMonth.querySelector(`.${config.CSS_CLASS_ROW_DAYS_OF_MONTH} 
+                                                   .${config.CSS_CLASS_DAY_OF_MONTH}.${config.CSS_CLASS_SELECTED}`)) {
 
-                    document.querySelectorAll('input[type="date_picker"]').forEach(input => {
+                        daysOfMonth.querySelector(`.${config.CSS_CLASS_ROW_DAYS_OF_MONTH} 
+                                                   .${config.CSS_CLASS_DAY_OF_MONTH}.${config.CSS_CLASS_SELECTED}`)
+                        .classList.remove(config.CSS_CLASS_SELECTED);
+                    }
+
+                    document.querySelectorAll(config.SELECTOR_INPUT_DATE_PICKER).forEach(input => {
                         if(input.dataset.id === daysOfMonth.dataset.id){
                             const selectedDate = day.dataset.date.split('-');
 
                             selectedDate[1] = `${+selectedDate[1] + 1}`;
 
-                            if(+selectedDate[1] < 10) selectedDate[1] = '0' + selectedDate[1];
+                            if(+selectedDate[1] < 10) { selectedDate[1] = '0' + selectedDate[1]; }
 
                             input.value = `${selectedDate[0]}-${selectedDate[1]}-${selectedDate[2]}`;
 
-                            day.classList.add('selected');
-                            input.blur()
+                            day.classList.add(config.CSS_CLASS_SELECTED);
+                            input.blur();
                         }
                     })
                 });
