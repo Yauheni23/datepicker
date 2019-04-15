2304deece82c0d20e2ca989e13f3b25319de5d8d
Yauheni23 <yauheni.yarmolich@mail.ru>
2019-04-15 12:54:08 +0300

    fix: date in dialog

--- src/dialog.js
+++ src/dialog.js
@@ -50,7 +50,7 @@ export class Dialog {
         buttonAddTime.className = `${config.CSS_CLASS_BUTTON_ADD_TIME} ${config.CSS_CLASS_ACTIVE} btn btn-outline-primary `;
         buttonAddTime.innerHTML = '<i class="fas fa-plus"></i><span> Add time</span>';
 
-        this.durationTask = 86400000;
+        this.durationTask = 0;
 
         const durationTask = {
             dayInMilliseconds: 0,
@@ -74,54 +74,43 @@ export class Dialog {
             }
         });
 
+        /* Работает */
         this.endTimeDatepicker.input.addEventListener('blur', () => {
-            if (this.endTimeDatepicker.input.value !== this.endDate.formatForInput()){
+            if (this.endTimeDatepicker.datepicker.date.selectedDate.formatForInput() !== this.endDate.formatForInput()){
+                const end = new DateForMonth(
+                    this.endDate.getFullYear(),
+                    this.endDate.getMonth(),
+                    this.endDate.getDate()
+                );
+                this.durationTask += this.endTimeDatepicker.datepicker.date.selectedDate - end;
                 this.updateDate();
-                durationTask.dayInMilliseconds = this.endDate - this.startDate;
 
-                if (durationTask.day() === 0) {
-                    durationTask.hours = 0;
-                    durationTask.minutes = 0;
-                    durationTask.dayInMilliseconds = 0;
-                    this.durationTask = 0;
-
-                    this.time.end.replaceSelectTime(+this.time.begin.hours, +this.time.begin.minutes);
-                    this.time.end.input.value = new DateForMonth(2019, 1, 1,
-                        +this.time.begin.hours,
-                        +this.time.begin.minutes)
-                        .formatForInputTime();
-                    this.updateDate();
-                }
-
-                if (this.startDate > this.endDate) {
-                    if (this.time) {
+                if (this.durationTask >= 0) {
+                    if(this.time && this.endTimeDatepicker.datepicker.date.selectedDate.formatForInput()
+                        === this.startTimeDatepicker.datepicker.date.selectedDate.formatForInput()) {
+                        this.time.end.replaceSelectTime(+this.time.begin.hours, +this.time.begin.minutes);
+                    } else {
+                        if(this.time) {
+                            this.time.end.replaceSelectTime(0, 0, false);
+                        }
+                    }
+                    this.enableSaveButton();
+                } else {
+                    if(this.time) {
                         this.time.end.replaceSelectTime(0, 0, false);
-                        this.time.end.input.value = new DateForMonth(2019, 1, 1,
-                            +this.time.begin.hours,
-                            +this.time.begin.minutes)
-                            .formatForInputTime();
                     }
                     this.disableSaveButton();
-                    this.updateDate();
-                } else {
-                    this.enableSaveButton();
                 }
             }
         });
 
         buttonAddTime.addEventListener(config.EVENT_LISTENER_CLICK, () => {
             this.time = {};
-            durationTask.dayInMilliseconds = 3600000;
             this.durationTask = 3600000;
-            durationTask.hours = 1;
-            durationTask.minutes = 0;
-            this.time.duration = {};
-            this.time.duration.hours = 1;
-            this.time.duration.minutes = 0;
 
             this.time.begin = new InputTime();
             this.time.end = new InputTime({
-                addHours: durationTask.hours,
+                addHours: 1,
                 defaultHours: this.time.begin.hours,
                 defaultMinutes: this.time.begin.minutes,
                 useDuration: true
@@ -129,64 +118,52 @@ export class Dialog {
 
             this.endTimeDatepicker.datepicker.date.selectedDate =
                 this.startTimeDatepicker.datepicker.date.selectedDate;
+
             this.endTimeDatepicker.datepicker.replaceDate({
                 year: this.endTimeDatepicker.datepicker.date.selectedDate.getFullYear(),
                 month: this.endTimeDatepicker.datepicker.date.selectedDate.getMonth(),
                 day: this.endTimeDatepicker.datepicker.date.selectedDate.getDate()
             });
             this.updateDate();
-
-
-
-            if (durationTask.day() < 0) {
-                this.time.end.replaceSelectTime(+this.time.begin.hours, +this.time.begin.minutes, false);
-                this.time.end.input.value = new DateForMonth(2019, 1, 1,
-                    +this.time.begin.hours,
-                    +this.time.begin.minutes)
-                    .formatForInputTime();
-                this.disableSaveButton();
-                this.updateDate();
-            } else {
-                this.enableSaveButton();
-            }
+            this.enableSaveButton();
 
             /* Работает.*/
             this.time.begin.input.addEventListener( 'focus', () => {
                 if (`${this.time.begin.hours}:${this.time.begin.minutes}` !== this.startDate.formatForInputTime()) {
-                    this.time.end.input.value = new DateForMonth(2019, 1, 1,
-                        +this.time.begin.hours + +durationTask.hours,
-                        +this.time.begin.minutes + +durationTask.minutes)
+                    console.log(new Date().setTime(this.convertInMilliseconds(+this.time.begin.hours,+this.time.begin.minutes) + this.durationTask));
+                    this.time.end.input.value = new DateForMonth(this.convertInMilliseconds(+this.time.begin.hours,+this.time.begin.minutes) + this.durationTask - 3 * 3600000)
                         .formatForInputTime();
                     this.time.end.updateTime();
-
                     if (this.time.end.hours < this.time.begin.hours) {
                         if(this.time.end.params.useDuration) {
                             this.time.end.replaceSelectTime(0, 0, false);
                         }
                     }
-
                     this.updateDate();
 
                     if (this.startDate.formatForInput() === this.endDate.formatForInput()) {
                         this.time.end.replaceSelectTime(+this.time.begin.hours, +this.time.begin.minutes);
                     }
+
                     this.endTimeDatepicker.datepicker.replaceDate({
                         year: this.endDate.getFullYear(),
                         month: this.endDate.getMonth(),
                         day: this.endDate.getDate()
                     });
 
+
                 }
             });
 
+            /* Работает.*/
             this.time.end.input.addEventListener( 'focus', () => {
                 if (`${this.time.end.hours}:${this.time.end.minutes}` !== this.endDate.formatForInputTime()) {
-                    this.updateDate();
-                    durationTask.hours = new Date(this.endDate - this.startDate).getUTCHours();
-                    durationTask.minutes = new Date(this.endDate - this.startDate).getUTCMinutes();
 
-                    if (this.time.end.hours < this.time.begin.hours && this.time.end.params.useDuration){
-                        durationTask.dayInMilliseconds += 86400000;
+                    this.durationTask += this.convertInMilliseconds(+this.time.end.hours - +this.endDate.getHours(), +this.time.end.minutes - +this.endDate.getMinutes());
+                    this.updateDate();
+
+                    if (+this.time.end.hours < +this.time.begin.hours && this.time.end.params.useDuration){
+                        this.durationTask += 86400000;
                         this.endTimeDatepicker.datepicker.date.selectedDate.setMilliseconds(86400000);
                         this.endTimeDatepicker.datepicker.replaceDate({
                             year: this.endTimeDatepicker.datepicker.date.selectedDate.getFullYear(),
@@ -195,8 +172,9 @@ export class Dialog {
                         });
                         this.time.end.replaceSelectTime(0, 0, false);
                     }
-
                     this.updateDate();
+                    console.log(this.durationTask);
+
                 }
             });
 
@@ -224,6 +202,7 @@ export class Dialog {
                 console.log(nameTask.value);
                 console.log(this.startDate);
                 console.log(this.endDate);
+                console.log(this.durationTask);
                 this.clearDialog(div, nameTask, buttonAddTime);
             } else {
                 nameTask.classList.add(config.CSS_CLASS_ERROR_INPUT);
@@ -299,6 +278,7 @@ export class Dialog {
     }
 
     updateDate() {
+
         this.startDate.setFullYear(this.startTimeDatepicker.datepicker.date.selectedDate.getFullYear());
         this.startDate.setMonth(this.startTimeDatepicker.datepicker.date.selectedDate.getMonth());
         this.startDate.setDate(this.startTimeDatepicker.datepicker.date.selectedDate.getDate());
@@ -306,9 +286,8 @@ export class Dialog {
         this.endDate.setFullYear(this.startTimeDatepicker.datepicker.date.selectedDate.getFullYear());
         this.endDate.setMonth(this.startTimeDatepicker.datepicker.date.selectedDate.getMonth());
         this.endDate.setDate(this.startTimeDatepicker.datepicker.date.selectedDate.getDate());
-        // this.endDate.setFullYear(this.endTimeDatepicker.datepicker.date.selectedDate.getFullYear());
-        // this.endDate.setMonth(this.endTimeDatepicker.datepicker.date.selectedDate.getMonth());
-        // this.endDate.setDate(this.endTimeDatepicker.datepicker.date.selectedDate.getDate());
+
+
 
         if(this.time) {
             this.time.end.updateTime();
@@ -316,31 +295,10 @@ export class Dialog {
             this.startDate.setMinutes(this.time.begin.minutes);
             this.endDate.setHours(this.time.begin.hours);
             this.endDate.setMinutes(this.time.begin.minutes);
-            // this.endDate.setHours(this.time.begin.hours);
-            // this.endDate.setMinutes(this.time.begin.minutes);
+
             this.endDate.setMilliseconds(this.durationTask);
         }
 
-        // if(this.time) {
-        //     this.time.end.updateTime();
-        //     this.startTimeDatepicker.datepicker.date.selectedDate.setHours(this.time.begin.hours);
-        //     this.startTimeDatepicker.datepicker.date.selectedDate.setMinutes(this.time.begin.minutes);
-        //     this.endTimeDatepicker.datepicker.date.selectedDate.setHours(this.time.end.hours);
-        //     this.endTimeDatepicker.datepicker.date.selectedDate.setMinutes(this.time.end.minutes);
-        // }
-        //
-        // this.startDate = new DateForMonth(this.startTimeDatepicker.datepicker.date.selectedDate.getFullYear(),
-        //     this.startTimeDatepicker.datepicker.date.selectedDate.getMonth(),
-        //     this.startTimeDatepicker.datepicker.date.selectedDate.getDate(),
-        //     this.startTimeDatepicker.datepicker.date.selectedDate.getHours(),
-        //     this.startTimeDatepicker.datepicker.date.selectedDate.getMinutes(),
-        // );
-        // this.endDate = new DateForMonth(this.endTimeDatepicker.datepicker.date.selectedDate.getFullYear(),
-        //     this.endTimeDatepicker.datepicker.date.selectedDate.getMonth(),
-        //     this.endTimeDatepicker.datepicker.date.selectedDate.getDate(),
-        //     this.endTimeDatepicker.datepicker.date.selectedDate.getHours(),
-        //     this.endTimeDatepicker.datepicker.date.selectedDate.getMinutes(),
-        // );
     }
 
     enableSaveButton() {
